d535226cda015dc80a23d382d692e2030514cef3
nathan79-c <19kt210@esissalma.org>
2024-09-01 15:53:57 +0200

    modification du theme

--- app/src/main/java/com/example/animation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/animation/ui/theme/Theme.kt
@@ -9,18 +9,32 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Navy,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+
+    surface = Blue,
+    onSurface = Navy,
+    onPrimary = Chartreuse
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = LightBlue,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+
+    surface = Blue,
+    onSurface = Color.White,
+    onPrimary = Navy
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -49,7 +63,13 @@ fun AnimationTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
