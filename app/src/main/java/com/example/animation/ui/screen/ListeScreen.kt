923d062d5e23fa538b7281b3a19fafac389b30db
nathan79-c <19kt210@esissalma.org>
2024-09-20 21:47:26 +0200

    creation de liste deroulante

---
+++ app/src/main/java/com/example/animation/ui/screen/ListeScreen.kt
@@ -0,0 +1,76 @@
+package com.example.animation.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProductListWithColumn() {
+    Column {
+        for (i in 1..100) {
+            ProductItem("Produit $i", "Description du produit $i")
+        }
+    }
+}
+
+@Composable
+fun ProductItem(name: String, description: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .background(Color.LightGray, RoundedCornerShape(8.dp))
+            .padding(16.dp)
+    ) {
+        Text(text = name, style =MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = description)
+    }
+}
+
+@Composable
+fun ProductListWithLazyColumn() {
+    LazyColumn {
+        items(100) { index ->
+            ProductItem("Produit $index", "Description du produit $index")
+        }
+    }
+}
+@Composable
+fun ProductListWithLazyRow() {
+    LazyRow {
+        items(100) { index ->
+            ProductItem("Produit $index", "Description du produit $index")
+        }
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun ProductListWithColumnPreview() {
+    ProductListWithColumn()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProductListWithLazyColumnPreview() {
+    ProductListWithLazyColumn()
+}
+@Preview(showBackground = true)
+@Composable
+fun ProductListWithLazyRowPreview() {
+    ProductListWithLazyRow()
+}
